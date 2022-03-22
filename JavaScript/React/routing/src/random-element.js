5e70d94ccbc8796febd2da2328a4f3106059aa01
sahilbaig <sahilbaig.me@gmail.com>
2022-03-22 15:12:59 +0530

    Hooks and Routing added

---
+++ JavaScript/React/routing/src/random-element.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Component } from "react";
+
+const Random = () =>{
+    return(
+        <div>
+            <h1>Random stuufs</h1>
+        </div>
+    )
+}
+
+export default Random
